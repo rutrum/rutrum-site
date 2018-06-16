6d2a5bb7330d0913e031dc659d351ed26e6d79e6
David Purdum <purdum41@gmail.com>
2018-06-16 15:50:55 -0400

    Add multiline strings and adv pointer advancement

--- rut-web-markup/read.py
+++ rut-web-markup/read.py
@@ -1,10 +1,10 @@
 class Read:
 
-    specials = [".", "#", "^", "|", "(", ")", "\"", "{", "}", "$", "*", "_"]
+    specials = [".", "#", "^", "|", "(", ")", "\"", "{", "}", "$", "*", "_", "/"]
 
     def __init__(self, filename):
-        self.filename = filename
-        self.reader = open(filename, "r")
+        self.filename = filename + ".rutwm"
+        self.reader = open(self.filename, "r")
         self.line = self.reader.readline()
         self.p = 0
         self.use_last = False
@@ -21,8 +21,10 @@ class Read:
         # If expected token is a string (or end of one)
         if self.last == "\"":
             if self.found_quotes:
+                # Second quotes
                 self.found_quotes = False
             else:
+                # First quotes
                 self.found_quotes = True
                 self.last = ""
                 return self.return_string("\"")
@@ -31,11 +33,6 @@ class Read:
         if self.last == "(":
             self.last = ""
             return self.return_string(")")
-
-        # If pointer is past line, get new line
-        if self.p >= len(self.line) - 1: # -1 ignores \n
-            self.line = self.reader.readline()
-            self.p = 0
         
         # If end of file, return empty string
         if self.line == "":
@@ -52,14 +49,17 @@ class Read:
             if self.is_special(char):
                 if self.last == "":
                     # Only seen special
-                    self.p += 1
+                    if char == "/" and self.line[self.p + 1] == "/": # should break when single / at endline
+                        self.goto_next_line()
+                        return self.next_token()
+                    self.advance_pointer()
                     self.last = char
                     return char
                 # Already read non-special characters
                 return self.last
 
             elif char == " ":
-                self.p += 1
+                self.advance_pointer()
                 # Ignores tabs/spaces at beginning of lines
                 if self.last == "":
                     continue
@@ -68,10 +68,11 @@ class Read:
             else:
                 # Character is not special or space
                 self.last += char
-                self.p += 1
+                self.advance_pointer()
 
         # If returning trailing spaces, try again with next line
         if self.last == "":
+            self.goto_next_line()
             return self.next_token()
 
         # Reached end of line; return what was found
@@ -81,7 +82,7 @@ class Read:
         char = self.line[self.p]
         while (char != expected and self.p < len(self.line) - 1):
             self.last += char
-            self.p += 1
+            self.advance_pointer()
             char = self.line[self.p]
         return self.last
 
@@ -94,3 +95,12 @@ class Read:
 
     def undo_token(self):
         self.use_last = True
+
+    def advance_pointer(self):
+        self.p += 1
+        if self.p >= len(self.line) - 1: # -1 ignores \n
+            self.goto_next_line()
+
+    def goto_next_line(self):
+        self.line = self.reader.readline()
+        self.p = 0
